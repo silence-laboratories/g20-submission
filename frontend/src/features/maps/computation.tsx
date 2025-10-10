7c0b22df84ffa295518ef2708a386c8b7844eb4d
Aditya Gannavarapu <adityagannavarapu.67@gmail.com>
2025-10-10 07:56:36 +0530

    Update link

--- frontend/src/features/maps/computation.tsx
+++ frontend/src/features/maps/computation.tsx
@@ -5,5 +5,5 @@ import { Card, CardContent } from '@/components/ui/card';
 
 export default function Computation() {
   return <Card>
-    <CardContent><video src='/assets/compute-vmake.mp4' autoPlay loop muted /></CardContent></Card>;
+    <CardContent><video src='/assets/compute.mp4' autoPlay loop muted /></CardContent></Card>;
 }
