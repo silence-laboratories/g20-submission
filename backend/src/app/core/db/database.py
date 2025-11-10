338d5234de60d0d5f1c65632ca79523b620534de
Aditya Gannavarapu <adityagannavarapu.67@gmail.com>
2025-11-10 13:18:20 +0530

    Update DB pool pre-ping

--- backend/src/app/core/db/database.py
+++ backend/src/app/core/db/database.py
@@ -15,7 +15,7 @@ DATABASE_URI = settings.POSTGRES_URI
 DATABASE_PREFIX = settings.POSTGRES_ASYNC_PREFIX
 DATABASE_URL = f"{DATABASE_PREFIX}{DATABASE_URI}"
 
-async_engine = create_async_engine(DATABASE_URL, echo=False, future=True)
+async_engine = create_async_engine(DATABASE_URL, echo=False, future=True, pool_pre_ping=True, pool_recycle=3600)
 
 local_session = async_sessionmaker(bind=async_engine, class_=AsyncSession, expire_on_commit=False)
 
